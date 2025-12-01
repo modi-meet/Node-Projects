6e4f2cd095be1f799c5dacea1c5cb988b5dcf326
Modi Meet <meetmodi8080@gmail.com>
2025-12-01 22:07:41 +0530

    Added validation rules and added TTL in URL model

--- proj04/models/url.js
+++ proj04/models/url.js
@@ -12,17 +12,25 @@ const urlSchema = new mongoose.Schema({
     },
     visitHistory: [{ // array of objects
         timestamp: { 
-            type: Number
+            type: Date,
+            default: Date.now
         }
     }],  
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users',
+    },
+    expiresAt: {
+        type: Date,
+        default: () => Date.now() + 24 * 60 * 60 * 1000  // delete after 24h
     }
-    
 }, 
     {timestamps: true}
 );
+
+// TTL index
+urlSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
 
 const URL = mongoose.model("url", urlSchema);
 
--- proj04/routes/url.js
+++ proj04/routes/url.js
@@ -1,8 +1,14 @@
+const { body, validationResult } = require('express-validator');
+
 const express = require('express');
 const { handleGenerateShortUrl, handleGetAnalytics } = require('../controller/url');
 const router = express.Router();
 
-router.post('/', handleGenerateShortUrl);
+router.post('/', [
+   body('url')
+      .notEmpty().withMessage('URL is required')
+      .isURL().withMessage('Invalid URL format')
+], handleGenerateShortUrl);
 
 router.get('/analytics/:shortId', handleGetAnalytics);
 
