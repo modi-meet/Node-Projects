7cd0b1425284f2b9791342e193781642c24d197b
Modi Meet <meetmodi8080@gmail.com>
2025-12-12 18:44:07 +0530

    Add new project - Chat Application Backend

---
+++ proj08-chatApp/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on('connection', (socket)=>{
+    socket.on('user-message', message=>{
+        io.emit('message', message);
+    });
+});
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req, res) => {
+    return re.sendFile('/public/index.html');
+});
+
+server.listen(9000, ()=> console.log("Server started at PORT: 9000"));
+
+
